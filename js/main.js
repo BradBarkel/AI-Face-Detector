5cd3da14d5aa5edd245e2c432a379857dea47f7a
Brad Barkel <bradleybarkel@gmail.com>
2020-02-14 16:51:34 -0700

    Completed javascript and added additional styling elements.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const video = document.getElementById("video");
+let predictedAges = [];
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
@@ -15,5 +16,60 @@ function startVideo () {
         { video: {}},
         stream => (video.srcObject = stream),
         err => console.error(err)
-    )
+    );
+}
+
+video.addEventListener('playing', () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+
+    const displaySize = 
+    { 
+        width: video.width, 
+        height: video.height
+    };
+
+    faceapi.matchDimensions(canvas, displaySize);
+
+    setInterval( async () => {
+        //Detect all faces
+        const detections = await faceapi
+        
+        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        //include facial landmarks
+        .withFaceLandmarks()
+        //include facial expressions
+        .withFaceExpressions()
+        .withAgeAndGender();
+
+
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        //clear canvas after each draw
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        //draw detected faces
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        //draw facial landmarks
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        //draw facial expressions
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        //draw calculated age
+        const age = resizedDetections[0].age;
+        const interpolatedAge = interpolateAgePredictions(age);
+        const bottomRight = {
+            x: resizedDetections[0].detection.box.bottomRight.x -60,
+            y: resizedDetections[0].detection.box.bottomRight.y
+        }
+        //draw interpolated age
+new faceapi.draw.DrawTextField(
+    [`${faceapi.utils.round(interpolatedAge, 0)} years`],
+    bottomRight).draw(canvas);
+
+    }, 100);
+});
+//function to return average of predicted age
+function interpolateAgePredictions (age) {
+    predictedAges = [age].concat(predictedAges).slice(0,30);
+    const avgPredictedAge =
+    predictedAges.reduce((total, a) => total + a) / predictedAges.length;
+    return avgPredictedAge;
 }
